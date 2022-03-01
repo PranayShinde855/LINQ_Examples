3d629816113d99015f7cb20024665c0e8adb661d
pranayshinde855 <pranayshinde855@gmail.com>
2022-03-01 17:19:16 +0530

    Add project files.

---
+++ LINQ_Practice/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace LINQ_Practice_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] myarr = { "Pranay", "Shinde", "Test" };
+
+            var getName = from p in myarr
+                          where p.Contains("a")
+                          select p;
+
+            foreach(var v in getName)
+            {
+                Console.WriteLine(v);
+            }
+
+            
+        }
+    }
+}
---
+++ LINQ_Practice_Models/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LINQ_Practice_Models
+{
+    class Program
+    {
+        //
+        static void Main(string[] args)
+        {
+            Person[] person =
+            {
+                new Person(){ Id=1, Name= "Pranay", Address="Roha"},
+                new Person(){ Id=2, Name= "", Address="Roha"},
+            };
+        }
+    }
+
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}
